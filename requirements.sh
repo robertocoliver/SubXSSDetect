5faf1cf5b407a289f8a91dc67c7ec662ec4905f3
Roberto Oliveira <robertocoliver@protonmail.com>
2024-03-23 00:26:33 -0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -2,6 +2,8 @@
 
 # Instala httpx:
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install github.com/projectdiscovery/katana/cmd/katana@latest
+go install github.com/tomnomnom/waybackurls@latest
 
 # Adicionar o diretório bin do Go ao PATH:
 export PATH=$PATH:/root/go/bin
